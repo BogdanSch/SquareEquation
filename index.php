82eaaf23098cee0aa31f18848b88be79c529c370
Bohdan <bogmira777@gmail.com>
2022-09-21 20:27:58 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = rand(-10, 10);
+$b = rand(-10, 10);
+$c = rand(-10, 10);
+
+$d = $b * $b - 4 * $a * $c;
+
+if($d < 0){
+    echo "<div style=\"color: red;\"> D < 0 <strong>There are no solutions!</strong></div>";
+}
+elseif($d == 0){
+    $x = sqrt($d) / (2 * $a);
+
+    echo "<div style=\"color: blue;\"> D = 0 <strong>x = $x </strong></div>";
+}
+else{
+    $x1 = (-$b + sqrt($d)) / (2 * $a);
+    $x2 = (-$b - sqrt($d)) / (2 * $a);
+
+    echo "<div style=\"color: blue;\"> D > 0 <strong>x1 = $x1, \n x2 = $x2 </strong></div>";
+}
